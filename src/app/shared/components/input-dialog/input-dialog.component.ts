177b6b214ce3dc6e2a5e3918383cbe20c406702e
Kely Rodríguez <mishelrodri.dev@gmail.com>
2025-04-02 17:51:25 -0600

    implementación de IconComponent en los modales

--- src/app/shared/components/input-dialog/input-dialog.component.ts
+++ src/app/shared/components/input-dialog/input-dialog.component.ts
@@ -6,6 +6,8 @@ import { ButtonModule } from 'primeng/button';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TextareaModule } from 'primeng/textarea';
+import { IconComponent } from '../icon/icon.component';
+import { IconType } from '@shared/interfaces/confirm-dialog.interface';
 
 @Component({
   selector: 'app-input-dialog',
@@ -15,6 +17,7 @@ import { TextareaModule } from 'primeng/textarea';
     InputTextModule,
     TextareaModule,
     ButtonModule,
+    IconComponent,
   ],
   templateUrl: './input-dialog.component.html',
   styleUrl: './input-dialog.component.scss',
@@ -23,7 +26,7 @@ export class InputDialogComponent {
   ref = inject(DynamicDialogRef);
   private config = inject(DynamicDialogConfig) as InputDialogData;
 
-  icon = this.config.data.icon;
+  icon = (this.config.data.icon ?? 'success') as IconType;
   iconColor = this.config.data.iconColor;
   title = this.config.data.title;
   label = this.config.data.label;
