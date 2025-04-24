2d8d575fe5258e6acd025a9f273cbd03fef71253
nayosx <nayos144@gmail.com>
2025-04-24 08:29:43 -0600

    set sessionID and transactionID

--- src/app/core/interceptors/general-request.interceptor.ts
+++ src/app/core/interceptors/general-request.interceptor.ts
@@ -1,19 +1,39 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 import dayjs from 'dayjs';
-import { inject } from "@angular/core";
-import { DeviceService } from "@shared/services/devices/device.service";
+import { customAlphabet } from 'nanoid';
 
-const pathsToInclude = ['/testGET', '/testPOST'];
+const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+const nano16 = customAlphabet(alphabet, 16);
 
+const nonZero = '123456789';
+const nanoidFirst = customAlphabet(nonZero, 1);
+
+const digits = '0123456789';
+const nanoidRest = customAlphabet(digits, 15);
+
+export function generateNumericId(): string {
+  return `${nanoidFirst()}${nanoidRest()}`;
+}
+
+const pathsToInclude = [
+  '/testGET', 
+  '/testPOST'
+];
 
 export function generalRequest(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  const deviceServ = inject(DeviceService);
+
+  let sessionId = sessionStorage.getItem('sid');
+
+  if (!sessionId) {
+    sessionId = generateNumericId();
+    sessionStorage.setItem('sid', sessionId);
+  }
 
   if (pathsToInclude.some(path => req.url.includes(path))) {
     const constantHeader = {
-      idTransaccion: 'bdf5fd9c-bf6a-4d60-8086-a',
-      idSesion: 124999519233026,
+      idTransaccion: nano16(),
+      idSesion: sessionId,
       fechaHora: dayjs().format('YYYY-MM-DD HH:mm:ss'),
       usuario: 'usuarioDemo',
       canal: 'PYME',
